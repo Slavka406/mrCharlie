3f66afd07273a75294dfa66f3e07d6a7fde9e303
Slavka406 <slavka1406@gmail.com>
2021-09-23 21:06:29 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,70 @@
-import { Tabs, Typography, Row, Col, Image, Grid } from 'antd';
+import { Tabs, Typography, Image, Grid, Dropdown, Button, Menu } from 'antd';
 import 'antd/dist/antd.css';
 import "animate.css"
+import { useState } from 'react'
+import { DownOutlined } from '@ant-design/icons';
+import { Row, Col, } from '@qonsoll/react-design'
+
+const UA = 'UA'
+const EN = 'EN'
+
+const menuUA = [
+  "menuUA/Str1.jpg",
+  "menuUA/str2.jpg",
+  "menuUA/str3.jpg",
+  "menuUA/str4.jpg",
+  "menuUA/str5.jpg",
+  "menuUA/str6.jpg",
+  "menuUA/str7.jpg",
+  "menuUA/str8.jpg",
+  "menuUA/str9.jpg",
+  "menuUA/str10.jpg",
+  "menuUA/str11.jpg",
+  "menuUA/str12.jpg",
+  "menuUA/str13.jpg",
+  "menuUA/str14.jpg"
+]
+
+const menuEN = [
+  "menuEN/Str1.jpg",
+  "menuEN/str2.jpg",
+  "menuEN/str3.jpg",
+  "menuEN/str4.jpg",
+  "menuEN/str5_.jpg",
+  "menuEN/str6_.jpg",
+  "menuEN/str7_.jpg",
+  "menuEN/str8_.jpg",
+  "menuEN/str9_.jpg",
+  "menuEN/str10_.jpg",
+  "menuEN/str11_.jpg",
+  "menuEN/str12_.jpg",
+  "menuEN/str13_.jpg",
+  "menuEN/str14_.jpg"
+]
+
+const alcogol = [
+  "alcogol/Alkogol1.jpg",
+  "alcogol/Alkogol2.jpg",
+  "alcogol/Alkogol3.jpg",
+  "alcogol/Alkogol4.jpg"
+]
+
+const banquet = [
+  "banquet/banquet0.jpg",
+  "banquet/banquet1.jpg",
+  "banquet/banquet2.jpg",
+  "banquet/banquet3.jpg"
+]
+
+const wine = [
+  "wine/redWine.jpg",
+  "wine/whiteWine.jpg"
+]
+
+const menuMap = {
+  EN: menuEN,
+  UA: menuUA
+}
 
 const { TabPane } = Tabs;
 const { Title } = Typography;
@@ -8,45 +72,36 @@ const { useBreakpoint } = Grid;
 
 function App() {
   const screens = useBreakpoint();
+  const [language, setLanguage] = useState(UA)
 
-  function callback(key) {
-    console.log(key); 
-  }
-
-const menu = [
-  "mrCharlie/Str1.jpg",
-  "mrCharlie/str2.jpg",
-  "mrCharlie/str3.jpg",
-  "mrCharlie/str4.jpg",
-  "mrCharlie/str5.jpg",
-  "mrCharlie/str6.jpg",
-  "mrCharlie/str7.jpg",
-  "mrCharlie/str8.jpg",
-  "mrCharlie/str9.jpg",
-  "mrCharlie/str10.jpg",
-  "mrCharlie/str11.jpg",
-  "mrCharlie/str12.jpg",
-  "mrCharlie/str13.jpg",
-  "mrCharlie/str14.jpg"
-]
-
-const alcogol = [
-  "mrCharlie/Alkogol1.jpg",
-  "mrCharlie/Alkogol2.jpg",
-  "mrCharlie/Alkogol3.jpg",
-  "mrCharlie/Alkogol4.jpg"
-]
+  const menu = (
+  <Menu onClick={(e) => {setLanguage(e?.key)}}>
+    <Menu.Item key={EN} >
+      {EN}
+    </Menu.Item>
+    <Menu.Item key={UA} >
+      {UA}
+    </Menu.Item>
+  </Menu>
+);
 
   return (
-    <div>
-      <Row style={{display:'flex', padding: '8px 8px 0 8px', background: '#e2d2bf', marginBottom: '8px'}}>        
-        <Col >
-          <Title level={3} style={{color: '#522e24'}}>Mr. Charlie</Title>
+    <div style={{backgroundImage: '0001.jpg'}}>
+      <Row v='center' display='flex' style={{padding: '8px 8px 8px 8px', background: '#e2d2bf', marginBottom: '8px'}}>        
+        <Col>
+          <Title level={3} style={{color: '#522e24', marginBottom: 0}}>Mr. Charlie</Title>
+        </Col>
+        <Col cw='auto'>
+          <Dropdown overlay={menu}>
+            <Button>
+              {language} <DownOutlined />
+            </Button>
+          </Dropdown>
         </Col>
       </Row>
       <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} style={{display:'flex', justifyContent:'center', padding: '0 8px 0 8px'}}>
         <Col >
-          <Tabs type="card" defaultActiveKey="1" onChange={callback}>
+          <Tabs type="card" defaultActiveKey="1">
             <TabPane tab="Меню" key="1">
               <div 
                 className={'animate__animated  animate__backInRight'}
@@ -64,10 +119,9 @@ const alcogol = [
                         screens.sm ? '90%' :  
                         screens.xs && '100vw' 
                   }}>
-                   {menu.map(item => 
+                   {menuMap[language].map(item => 
                       <Image
                         preview={false}
-                        // className={'animate__animated  animate__backInRight'}
                         width='100%'
                         src={item}
                       />
@@ -75,7 +129,35 @@ const alcogol = [
                 </div>
               </div>
             </TabPane>
-            <TabPane tab="Алкоголь" key="2">
+            {/* <TabPane tab="Menu En" key="2">
+              <div 
+                className={'animate__animated  animate__backInRight'}
+                style={{ 
+                  display: 'flex',
+                  justifyContent: 'center'
+                }}>
+                <div 
+                  style={{ 
+                    overflow: 'auto',
+                    height: 'calc(100vh - 120px)',
+                    width: screens.xxl ? '50%' : 
+                        screens.xl ? '60%' : 
+                        screens.lg ? '70%' : 
+                        screens.md ? '80%' :  
+                        screens.sm ? '90%' :  
+                        screens.xs && '100%' 
+                  }}>
+                    {menuEN.map(item => 
+                        <Image
+                          preview={false}
+                          width='100%'
+                          src={item}
+                        />
+                      )}
+                </div>
+              </div>
+            </TabPane> */}
+            <TabPane tab="Алкоголь" key="3">
               <div 
                 className={'animate__animated  animate__backInRight'}
                 style={{ 
@@ -96,7 +178,6 @@ const alcogol = [
                     {alcogol.map(item => 
                         <Image
                           preview={false}
-                          // className={'animate__animated  animate__backInRight'}
                           width='100%'
                           src={item}
                         />
@@ -104,10 +185,68 @@ const alcogol = [
                 </div>
               </div>
             </TabPane>
+            <TabPane tab="Винa" key="5">
+              <div 
+                className={'animate__animated  animate__backInRight'}
+                style={{ 
+                  display: 'flex',
+                  justifyContent: 'center'
+                }}>
+                <div 
+                  style={{ 
+                    overflow: 'auto',
+                    height: 'calc(100vh - 120px)',
+                    width: screens.xxl ? '50%' : 
+                        screens.xl ? '60%' : 
+                        screens.lg ? '70%' : 
+                        screens.md ? '80%' :  
+                        screens.sm ? '90%' :  
+                        screens.xs && '100%' 
+                  }}>
+                    {wine.map(item => 
+                        <Image
+                          preview={false}
+                          width='100%'
+                          src={item}
+                        />
+                      )}
+                </div>
+              </div>
+            </TabPane>
+            <TabPane tab="Банкетне" key="4">
+              <div 
+                className={'animate__animated  animate__backInRight'}
+                style={{ 
+                  display: 'flex',
+                  justifyContent: 'center'
+                }}>
+                <div 
+                  style={{ 
+                    overflow: 'auto',
+                    height: 'calc(100vh - 120px)',
+                    width: screens.xxl ? '50%' : 
+                        screens.xl ? '60%' : 
+                        screens.lg ? '70%' : 
+                        screens.md ? '80%' :  
+                        screens.sm ? '90%' :  
+                        screens.xs && '100%' 
+                  }}>
+                    {banquet.map(item => 
+                        <Image
+                          preview={false}
+                          width='100%'
+                          src={item}
+                        />
+                      )}
+                </div>
+              </div>
+            </TabPane>
+          
+
             {/* <TabPane tab="Банкет" key="3">
               <Image
                 width='100%'
-                src="mrCharlie/Str1.jpg"
+                src="menuUA/Str1.jpg"
               />
             </TabPane> */}
           </Tabs>
